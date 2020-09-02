fcf18a1cf4840feb3290de617b998c8aea460a01
Antonio <antonioareas@me.com>
2020-09-01 18:03:07 -0700

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,51 @@
+﻿/// Week No. 2	Lab No. 1
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  September 1st, 2020
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the screen
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will do some math for you");
+            Console.WriteLine("Enter a number and press enter for each three times:");
+
+            // declare three integer variables and their math results
+            int n1, n2, n3;
+            int sum, product, quotient;
+
+            //Get user data for all 3 numbers
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+
+            // calculate the sum of these three numbers
+            sum = n1 + n2 + n3;
+
+            // print a message along with the sum of the two numbers
+            Console.WriteLine("The sum of those three numbers is: {0}\n", sum);
+
+            //Mulitply the 3 and print result
+            product = n1 * n2 * n3;
+            Console.WriteLine("The product of those three numbers is: {0}\n", product);
+
+            //divide the sum by product
+            quotient = sum / product;
+            Console.WriteLine("The sum divided by the product is: {0}\n", quotient);
+
+            //Just to pause the screen.
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,42 @@
+﻿/// Week No. 2	Lab No. 2
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  September 1st, 2020
+
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+
+        //declare cost of prizes
+        public const int CANDY_COST = 10;
+        public const int GUMBALL_COST = 3;
+
+        static void Main(string[] args)
+        {
+
+            //declare and determine amount of coupons
+            int coupons;
+            Console.WriteLine("How many coupons did you win? ");
+            coupons = int.Parse(Console.ReadLine());
+
+            //declare amount of gumballs and candy
+            int numOfGumballs, numOfCandy;
+
+            //determine how much candy you can buy
+            numOfCandy = coupons / CANDY_COST;
+
+            //remove coupons based on how many were bought
+            coupons = coupons % CANDY_COST;
+
+            //determine how many gumballs can be bought with remainder
+            numOfGumballs = coupons / GUMBALL_COST;
+
+            //tell the user how much they can buy
+            Console.WriteLine("You can buy {0} candy bars and {1} gumballs.", numOfCandy, numOfGumballs);
+
+        }
+    }
+}
